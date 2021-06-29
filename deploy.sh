49ed989b8bd566fa673c72c9bde50e4a1954ff82
yashbhangdia <55742719+yashbhangdia@users.noreply.github.com>
2021-06-29 11:10:46 +0530

    updated 1.0 to 1.3.5

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Shell Script to deploy Spring-Boot war file
 
-nohup java -jar SpringBootMavenExample-1.0.RELEASE.war >> app.log &
+nohup java -jar SpringBootMavenExample-1.3.5.RELEASE.war >> app.log &

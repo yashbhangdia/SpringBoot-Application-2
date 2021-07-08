bdf115326e9087a72017e6a244728a3c4741954e
yashbhangdia <55742719+yashbhangdia@users.noreply.github.com>
2021-07-08 15:28:09 +0530

    Update MyWebApplication.java

--- src/main/java/com/javabycode/springboot/MyWebApplication.java
+++ src/main/java/com/javabycode/springboot/MyWebApplication.java
@@ -5,6 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
 
 @SpringBootApplication
 public class MyWebApplication extends SpringBootServletInitializer{
@@ -14,7 +23,25 @@ public class MyWebApplication extends SpringBootServletInitializer{
     }
 
     public static void main(String[] args) throws Exception {
+        ArrayList<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+        int flag = 0;
+        for (NetworkInterface iface : interfaces)
+        {
+            for (InterfaceAddress addr : iface.getInterfaceAddresses()) 
+            {
+               if(addr.getAddress().toString().startsWith("/192.168."))
+               {
+                   flag=1;
+                   System.setProperty("server.address", addr.getAddress().toString().substring(1));
+                   break;
+               }
+                if(flag==1)
+                {
+                    break;
+                }
+            } 
+        }
+        
         SpringApplication.run(MyWebApplication.class, args);
     }
 }
-
